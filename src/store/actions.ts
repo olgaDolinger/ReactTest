56d361e3cf604ce6ecd075050ae04c9b089d1343
Ольга Долингер <helga@Air-Olga.7bitlabs.net>
2022-06-22 16:13:34 +0100

    trash removed

--- src/store/actions.ts
+++
@@ -1,9 +0,0 @@
-import { News } from './newsReducer';
-import { LOGIN } from './types';
-
-export const login = (payload: News) => {
-    return{
-        type: LOGIN,
-        payload: payload,
-    }
-}
